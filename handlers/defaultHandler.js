a9a6e7af6d925e16d8feb987af6ae09642b3e6da
anil-bomma <anil.bommaa@gmail.com>
2019-08-29 20:43:48 -0500

    - added defaultHandler.js file

---
+++ handlers/defaultHandler.js
@@ -0,0 +1,24 @@
+const config = require('../config.js');
+
+const defaultHandler = {
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = config.helpMessage;
+        const reprompt = config.helpReprompt;
+        this.response._responseObject.response.shouldEndSession = false
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response._responseObject.response.shouldEndSession = false
+        this.response.speak(config.stopMessage);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response._responseObject.response.shouldEndSession = false
+        this.response.speak(config.stopMessage);
+        this.emit(':responseReady');
+    },
+};
+
+module.exports = defaultHandler;
