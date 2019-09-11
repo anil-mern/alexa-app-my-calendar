ee876059389828e78a75a08d72dd027f67281050
anil-bomma <anil.bommaa@gmail.com>
2019-09-11 05:17:59 -0500

    - added new intent - modularized into folders

--- handlers/mainHandler.js
+++ handlers/mainHandler.js
@@ -2,9 +2,10 @@ let log4js = require('log4js'),
     util = require("util");
 
 let config = require('../config.js');
-let publicHoildays = require('./publicHoildays.json');
-let weeksFormat = require('./weeks.json');
-let coursesList = require('./faculty.json');
+let publicHoildays = require('../data/publicHoildays.json');
+let weeksFormat = require('../data/weeks.json');
+let coursesList = require('../data/faculty.json');
+let recList = require('../data/studentRec.json');
 
 let logger = log4js.getLogger('my-calendar.mainHandler');
 logger.level = 'debug';
@@ -38,17 +39,17 @@ var mainHandler = {
 
     // #2
     'MyClassesInfo': function () {
-        
+
         let responseText = "";
         let eventRequest = this.event.request;
         this.response._responseObject.response.shouldEndSession = false;
 
         let filledSlots = delegateSlotCollection(this, eventRequest);
 
-        if (filledSlots && (filledSlots.confirmationStatus === 'NONE' || 
-        filledSlots.confirmationStatus === 'CONFIRMED') && eventRequest.intent.slots && 
-        eventRequest.intent.slots.customDate.value) {
-            
+        if (filledSlots && (filledSlots.confirmationStatus === 'NONE' ||
+            filledSlots.confirmationStatus === 'CONFIRMED') && eventRequest.intent.slots &&
+            eventRequest.intent.slots.customDate.value) {
+
             let date = new Date(eventRequest.intent.slots.customDate.value);
             let todayDate = date.toLocaleDateString();
             let isHolidayAvailable = publicHoildays[todayDate];
@@ -57,8 +58,8 @@ var mainHandler = {
             if (isHolidayAvailable) {
 
                 responseText = util.format(
-                    "It seems like you don't have school on %s, because it's %s!", 
-                    todayDate, 
+                    "It seems like you don't have school on %s, because it's %s!",
+                    todayDate,
                     isHolidayAvailable
                 );
 
@@ -67,9 +68,9 @@ var mainHandler = {
                 logger.info(" Schedules has executed successfully, %s", responseText);
 
             } else if (weekCode == "0" || weekCode == "6") {
-                
+
                 responseText = util.format(
-                    "It's %s, there are no classes, school is off...", 
+                    "It's %s, there are no classes, school is off...",
                     weeksFormat[weekCode]
                 );
 
@@ -78,14 +79,15 @@ var mainHandler = {
                 logger.info(" Schedules has executed successfully");
 
             } else {
-                coursesList[weekCode%2].map(function(details) {
-                    responseText += util.format(" %s by %s from %s,", details.course, details.faculty, details.timing);
+                coursesList[weekCode % 2].map(function (details) {
+                    responseText += util.format(" %s by %s from %s at %s,", 
+                    details.course, details.faculty, details.timing, details.location);
                 });
 
                 responseText = util.format(
                     "On %s, you have %s sessions, %s",
                     todayDate,
-                    coursesList[weekCode%2].length,
+                    coursesList[weekCode % 2].length,
                     responseText
                 );
 
@@ -95,7 +97,7 @@ var mainHandler = {
                 this.emit(':responseReady');
                 logger.info(" Schedules has executed successfully");
             }
-            
+
         } else {
 
             let date = new Date();
@@ -106,8 +108,8 @@ var mainHandler = {
             if (isHolidayAvailable) {
 
                 responseText = util.format(
-                    "It seems like you don't have school on %s, because it's %s!", 
-                    todayDate, 
+                    "It seems like you don't have school on %s, because it's %s!",
+                    todayDate,
                     isHolidayAvailable
                 );
 
@@ -116,9 +118,9 @@ var mainHandler = {
                 logger.info(" Schedules has executed successfully, %s", responseText);
 
             } else if (weekCode == "0" || weekCode == "6") {
-                
+
                 responseText = util.format(
-                    "It's %s, there are no classes, school is off...", 
+                    "It's %s, there are no classes, school is off...",
                     weeksFormat[weekCode]
                 );
 
@@ -127,14 +129,15 @@ var mainHandler = {
                 logger.info(" Schedules has executed successfully");
 
             } else {
-                coursesList[weekCode%2].map(function(details) {
-                    responseText += util.format(" %s by %s from %s,", details.course, details.faculty, details.timing);
+                coursesList[weekCode % 2].map(function (details) {
+                    responseText += util.format(" %s by %s from %s at %s,", 
+                    details.course, details.faculty, details.timing, details.location);
                 });
 
                 responseText = util.format(
-                    "Today that is on %s, you have %s sessions, %s", 
+                    "Today that is on %s, you have %s sessions, %s",
                     todayDate,
-                    coursesList[weekCode%2].length,
+                    coursesList[weekCode % 2].length,
                     responseText
                 );
 
@@ -147,7 +150,45 @@ var mainHandler = {
         }
     },
 
-    // #3
+    // #2
+    'StudentRec': function () {
+
+        let responseText = "";
+        let eventRequest = this.event.request;
+        this.response._responseObject.response.shouldEndSession = false;
+
+        let date = new Date();
+        let todayDate = date.toLocaleDateString();
+        let isHolidayAvailable = publicHoildays[todayDate];
+        let weekCode = date.getDay();
+
+        if (isHolidayAvailable) {
+
+            responseText = util.format(
+                "Student Rec center will be remained closed on %s, because it's %s!",
+                todayDate,
+                isHolidayAvailable
+            );
+
+            this.response.speak(responseText);
+            this.emit(':responseReady');
+            logger.info(" Schedules has executed successfully, %s", responseText);
+
+        } else {
+
+            responseText += util.format("Student rec center timing on %s, is between %s,",
+                todayDate, recList[weekCode].timing);
+
+            this.response._responseObject.response.shouldEndSession = false;
+            this.response.cardRenderer(responseText);
+            this.response.speak(responseText);
+            this.emit(':responseReady');
+            logger.info(" Schedules has executed successfully");
+        }
+
+    },
+
+    // #4
     'Unhandled': function () {
         logger.info('Unhandled intent. Not match with any intent defined.');
         this.response._responseObject.response.shouldEndSession = false;
